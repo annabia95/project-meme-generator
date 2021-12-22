f2f0dc7c4f5777e33699c273a6c5642fa10e45b6
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-22 15:03:13 -0300

    Cumprindo o Segundo Requisito!

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const getInput = document.querySelector('#text-input');
 const texto = document.querySelector('#meme-text');
+const getInputImage = document.querySelector ('#meme-insert');
+const showImage = document.querySelector ('#meme-image');
+
 
 // Requisito 1 -  Usei como referência a documentação do evento 'keyup'. Link:https://developer.mozilla.org/pt-BR/docs/Web/API/Document/keyup_event
 function mostrarTexto() {
@@ -7,3 +10,10 @@ function mostrarTexto() {
 }
 
 getInput.addEventListener('keyup', mostrarTexto);
+
+// Requisito 2 - Usei como referência o seguinte vídeo: https://www.youtube.com/watch?v=io5FcMAdLyQ
+function insertMeme() {
+  const img = getInputImage.files;
+  showImage.src = URL.createObjectURL(img[0]);
+}
+getInputImage.addEventListener('change', insertMeme);
